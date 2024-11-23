2ea67fb30933fdb409094d3ccadc58916e914df8
1nFrastr <960843127@qq.com>
2024-11-23 20:58:20 +0800

    fix

--- app/Middleware/LikeAdminAllowMiddleware.php
+++ app/Middleware/LikeAdminAllowMiddleware.php
@@ -19,7 +19,7 @@ class LikeAdminAllowMiddleware
 
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: Authorization, Sec-Fetch-Mode, DNT, X-Mx-ReqToken, Keep-Alive, User-Agent, If-Match, If-None-Match, If-Unmodified-Since, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Accept-Language, Origin, Accept-Encoding,Access-Token,token,version");
-        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, post');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Max-Age: 1728000');
         header('Access-Control-Allow-Credentials:true');
 
