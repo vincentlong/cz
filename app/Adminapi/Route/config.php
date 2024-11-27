7f8b219614604e942b55005fa5880ef87053012c
1nFrastr <960843127@qq.com>
2024-11-27 15:42:44 +0800

    up

--- app/Adminapi/Route/config.php
+++ app/Adminapi/Route/config.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ConfigController::class)->group(function () {
     Route::get('/config/getConfig', 'getConfig');
+    Route::get('/config/dict', 'dict');
 });
