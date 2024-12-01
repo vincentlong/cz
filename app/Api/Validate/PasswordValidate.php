c2057dd10da9568eac443af5897016cab1902a76
1nFrastr <960843127@qq.com>
2024-12-01 12:21:57 +0800

    feat 用户修改密码

--- app/Api/Validate/PasswordValidate.php
+++ app/Api/Validate/PasswordValidate.php
@@ -1,69 +1,42 @@
 <?php
-// +----------------------------------------------------------------------
-// | likeadmin快速开发前后端分离管理后台（PHP版）
-// +----------------------------------------------------------------------
-// | 欢迎阅读学习系统程序代码，建议反馈是我们前进的动力
-// | 开源版本可自由商用，可去除界面版权logo
-// | gitee下载：https://gitee.com/likeshop_gitee/likeadmin
-// | github下载：https://github.com/likeshop-github/likeadmin
-// | 访问官网：https://www.likeadmin.cn
-// | likeadmin团队 版权所有 拥有最终解释权
-// +----------------------------------------------------------------------
-// | author: likeadminTeam
-// +----------------------------------------------------------------------
-namespace app\api\validate;
 
-use app\common\validate\BaseValidate;
+namespace App\Api\Validate;
 
-/**
- * 密码校验
- * Class PasswordValidate
- * @package app\api\validate
- */
+use App\Common\Validate\BaseValidate;
+
 class PasswordValidate extends BaseValidate
 {
-
-    protected $rule = [
-        'mobile' => 'require|mobile',
-        'code' => 'require',
-        'password' => 'require|length:6,20|alphaNum',
-        'password_confirm' => 'require|confirm',
-    ];
-
-
-    protected $message = [
-        'mobile.require' => '请输入手机号',
-        'mobile.mobile' => '请输入正确手机号',
-        'code.require' => '请填写验证码',
-        'password.require' => '请输入密码',
-        'password.length' => '密码须在6-25位之间',
-        'password.alphaNum' => '密码须为字母数字组合',
-        'password_confirm.require' => '请确认密码',
-        'password_confirm.confirm' => '两次输入的密码不一致'
-    ];
-
-
-    /**
-     * @notes 重置登录密码
-     * @return PasswordValidate
-     * @author 段誉
-     * @date 2022/9/16 18:11
-     */
-    public function sceneResetPassword()
+    public function rules($scene = '')
     {
-        return $this->only(['mobile', 'code', 'password', 'password_confirm']);
+        $rules = [
+            'resetPassword' => [
+                'mobile' => 'required|regex:/^1[3-9]\d{9}$/',
+                'code' => 'required',
+                'password' => 'required|string|between:6,20|alpha_num',
+                'password_confirm' => 'required|same:password',
+            ],
+            'changePassword' => [
+                'password' => 'required|string|between:6,20|alpha_num',
+                'password_confirm' => 'required|same:password',
+            ],
+        ];
+
+        return $rules[$scene] ?? [];
     }
 
+    protected $messages = [
+        'mobile.required' => '请输入手机号',
+        'mobile.regex' => '请输入正确手机号',
+        'code.required' => '请填写验证码',
+        'password.required' => '请输入密码',
+        'password.between' => '密码须在6-20位之间',
+        'password.alpha_num' => '密码须为字母数字组合',
+        'password_confirm.required' => '请确认密码',
+        'password_confirm.same' => '两次输入的密码不一致',
+    ];
 
-    /**
-     * @notes 修改密码场景
-     * @return PasswordValidate
-     * @author 段誉
-     * @date 2022/9/20 19:14
-     */
-    public function sceneChangePassword()
+    public function messages()
     {
-        return $this->only(['password', 'password_confirm']);
+        return $this->messages;
     }
-
 }
