3c4a5776b518b996d1bc420f6933f3147413ada1
1nFrastr <960843127@qq.com>
2024-12-23 20:20:25 +0800

    feat 安装引导

--- public/index.php
+++ public/index.php
@@ -12,6 +12,11 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+if (!file_exists(__DIR__ .'/../config/install.lock')) {
+    header("location:/install/install.php");
+    exit;
+}
+
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
---
+++ public/install/YxEnv.php
@@ -0,0 +1,52 @@
+<?php
+
+class YxEnv
+{
+    protected $filePath = '';
+
+    /**
+     * 读取环境变量定义文件
+     * @access public
+     * @param string $file 环境变量定义文件
+     * @return void
+     */
+    public function load($file)
+    {
+        $this->filePath = $file;
+    }
+
+    public function makeEnv($file)
+    {
+        if (!file_exists($file)) {
+            try {
+                touch($file);
+            } catch (Exception $e) {
+                return;
+            }
+        }
+    }
+
+    /**
+     * Notes: 写入Env文件
+     * @param $envFilePath
+     * @param array $databaseEnv
+     */
+    public function putEnv($envFilePath, array $databaseEnv)
+    {
+        $applyDbEnv = [
+            '{DB_HOST}' => $databaseEnv['host'],
+            '{DB_DATABASE}' => $databaseEnv['name'],
+            '{DB_USERNAME}' => $databaseEnv['user'],
+            '{DB_PASSWORD}' => $databaseEnv['password'],
+            '{DB_PORT}' => $databaseEnv['port'],
+            '{DB_PREFIX}' => $databaseEnv['prefix'],
+        ];
+        global $uniqueSalt;
+        $toReplace = array_merge($applyDbEnv, ['{PROJECT_UNIQUE_IDENTIFICATION}' => $uniqueSalt]);
+        $exampleEnv = file_get_contents($this->filePath);
+        foreach ($toReplace as $key => $value) {
+            $exampleEnv = str_replace($key, $value, $exampleEnv);
+        }
+        file_put_contents($envFilePath, $exampleEnv);
+    }
+}
